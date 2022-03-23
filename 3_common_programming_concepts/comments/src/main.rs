c7c3320d7982ae971bc77afeda9180c66597ddb9
jtabalon <joseph@tabalon.io>
2022-03-23 05:19:59 -0700

    complete comments section

--- 3_common_programming_concepts/comments/src/main.rs
+++ 3_common_programming_concepts/comments/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    // hello, world
+
+    // So we’re doing something complicated here, long enough that we need
+    // multiple lines of comments to do it! Whew! Hopefully, this comment will
+    // explain what’s going on.
+
+    // let lucky_number = 7; // I’m feeling lucky today
+
+    // More common format is to annotate on the line above it:
+
+    // I’m feeling lucky today
+    let lucky_number = 7; 
+
+
+
 }
