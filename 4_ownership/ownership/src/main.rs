73fc5250ec92fd9630e8eb22e0b8de9da54e6867
jtabalon <joseph@tabalon.io>
2022-03-29 05:37:43 -0700

    add mutable string

--- 4_ownership/ownership/src/main.rs
+++ 4_ownership/ownership/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
+    // s is not valid here, it's not yet declared
+    // let s = "hello";    s is valid from this point forward
 
-}
+    // do stuff with s
+
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); // push_str() appends a literal to a String
+
+    println!("{}", s); // This will print `hello, world!`
+
+
+} //scope is now over, and s is no longer valid
