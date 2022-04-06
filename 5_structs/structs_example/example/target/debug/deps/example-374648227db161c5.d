0144f604a0bbc2bcf2974341e502fd85d44f6eed
jtabalon <joseph@tabalon.io>
2022-04-06 05:33:15 -0700

    add and fix

---
+++ 5_structs/structs_example/example/target/debug/deps/example-374648227db161c5.d
@@ -0,0 +1,5 @@
+/Users/jtabalonjr/the_rust_book/5_structs/structs_example/example/target/debug/deps/example-374648227db161c5: src/main.rs
+
+/Users/jtabalonjr/the_rust_book/5_structs/structs_example/example/target/debug/deps/example-374648227db161c5.d: src/main.rs
+
+src/main.rs:
---
+++ 5_structs/structs_example/example/target/debug/example.d
@@ -0,0 +1,1 @@
+/Users/jtabalonjr/the_rust_book/5_structs/structs_example/example/target/debug/example: /Users/jtabalonjr/the_rust_book/5_structs/structs_example/example/src/main.rs
