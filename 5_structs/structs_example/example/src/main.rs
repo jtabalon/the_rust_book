b7591936ef22ed198fd84f1dbfad4d72e1d5e60f
jtabalon <joseph@tabalon.io>
2022-04-06 05:38:30 -0700

    add Debug macro and fmt Display

--- 5_structs/structs_example/example/src/main.rs
+++ 5_structs/structs_example/example/src/main.rs
@@ -26,7 +26,7 @@
 //     dimensions.0 * dimensions.1
 // }
 
-
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -37,6 +37,8 @@ fn main() {
         width: 30,
         height: 50,
     };
+
+    println!("rect1 is {:#?}", rect1);
 
     println!(
         "The area of the rectangle is {} square pixels.",
