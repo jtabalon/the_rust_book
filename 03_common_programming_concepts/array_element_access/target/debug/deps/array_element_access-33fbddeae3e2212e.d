189ab8f0c38b2dbc7fe4619eec05b1a2ce9778f6
jtabalon <joseph@tabalon.io>
2022-11-19 09:19:58 -0800

    rename directories

---
+++ 03_common_programming_concepts/array_element_access/target/debug/deps/array_element_access-33fbddeae3e2212e.d
@@ -0,0 +1,5 @@
+/Users/jtabalonjr/the_rust_book/3_common_programming_concepts/array_element_access/target/debug/deps/array_element_access-33fbddeae3e2212e: src/main.rs
+
+/Users/jtabalonjr/the_rust_book/3_common_programming_concepts/array_element_access/target/debug/deps/array_element_access-33fbddeae3e2212e.d: src/main.rs
+
+src/main.rs:
